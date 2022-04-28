da14e78c0ffce7189d04c26a481cfc5d1b7177ce
root <root@sabine.eecs.umich.edu>
2022-04-28 13:24:21 -0400

    application.rb file

--- config/application.rb
+++ config/application.rb
@@ -21,7 +21,7 @@ Rails.application.configure do
   config.datetime_format = '%l:%M %p on %B %e, %Y'
 
   # Uniqnames of DCO staff who can edit
-  config.administrators = [:victorbp, :laura, :don, :lrandolp, :hugh, :jvanlav, :liz, :dholt, :stander, :cpuzzuol, :pbhill, :gkroslak, :hayjacob, :jribben, :kowalskj]
+  config.administrators = [:victorbp, :laura, :don, :lrandolp, :hugh, :jvanlav, :liz, :dholt, :stander, :cpuzzuol, :pbhill, :gkroslak, :hayjacob, :jribben, :kowalskj, :josuegon]
 
   # number of items to show on index/listing pages (numbers greater than this
   # result in the page being split into multiple sub-pages)
